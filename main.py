07bc5d3bb639435898a0a60862db8bb61239da1f
RealBongoChongo <70061478+RealBongoChongo@users.noreply.github.com>
2021-01-04 13:06:24 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands 
+
+bc = commands.Bot(command_prefix='#')
+
+@bc.event()
+async def on_ready():
+  print('bot is ready')
+
+@bc.event()
+async def on_member_join(member):
+  print(f'{member} has joined a server with your bot')
+
+@bc.event()
+async def on_member_remove(member):
+  print(f'{member} has left a server with your bot')
+
+bc.run('YOUR_TOKEN_HERE')
