a798434cd5b90e1107c1d579dc39fa4efc47d2ca
RealBongoChongo <70061478+RealBongoChongo@users.noreply.github.com>
2021-01-04 13:13:29 -0600

    Episode 3 changes

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import discord
 from discord.ext import commands 
+import random
+from random import choice
 
 bc = commands.Bot(command_prefix='#')
 
@@ -15,4 +17,35 @@ async def on_member_join(member):
 async def on_member_remove(member):
   print(f'{member} has left a server with your bot')
 
+@bc.command(aliases=['eightball', '8ball'])
+async def _8ball(ctx, *, question):
+  responses = [
+      "It is certain.", "It is decidedly so.", "Without a doubt.",
+      "Yes - definitely.", "As I see it, yes.", "Most likely.",
+      "Outlook good.", "Yes.", "Signs point to yes.",
+      "Reply hazy, try again.", "Ask again later.",
+      "Better not tell you now.", "Concentrate and ask again.",
+      "Don't count on it.", "My reply is no.", "My sources say no.",
+      "Outlook not so good.", "Very doubtful."
+  ]
+  await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
+
+@bc.command()
+async def ping(ctx):
+  await ctx.send(f'Pong! `{round(self.bc.latency * 1000)}ms`')
+  
+@bc.command(pass_context=True)
+@commands.has_permissions(manage_guild=True)
+async def addrole(ctx, member:discord.Member, *, role:discord.Role = None):
+  await ctx.message.delete()
+  await member.add_roles(role)
+  await ctx.send(f'{member} Was Given {role}')
+
+@bc.command(pass_context=True)
+@commands.has_permissions(manage_guild=True)
+async def takerole(ctx, member:discord.Member, *, role:discord.Role = None):
+  await ctx.message.delete()
+  await member.remove_roles(role)
+  await ctx.send(f'{role} was taken from {member}')
+  
 bc.run('YOUR_TOKEN_HERE')
