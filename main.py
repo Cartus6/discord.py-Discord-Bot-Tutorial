4f42d55ede1ba520d26f8a83b73e1e19609130c7
RealBongoChongo <70061478+RealBongoChongo@users.noreply.github.com>
2021-01-04 13:07:26 -0600

    Updated from Episode-2

--- main.py
+++ main.py
@@ -15,4 +15,21 @@ async def on_member_join(member):
 async def on_member_remove(member):
   print(f'{member} has left a server with your bot')
 
+@bc.command(aliases=['eightball', '8ball'])
+async def _8ball(ctx, *, question):
+  responses = [
+      "It is certain.", "It is decidedly so.", "Without a doubt.",
+      "Yes - definitely.", "As I see it, yes.", "Most likely.",
+      "Outlook good.", "Yes.", "Signs point to yes.",
+      "Reply hazy, try again.", "Ask again later.",
+      "Better not tell you now.", "Concentrate and ask again.",
+      "Don't count on it.", "My reply is no.", "My sources say no.",
+      "Outlook not so good.", "Very doubtful."
+  ]
+  await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
+
+@bc.command()
+async def ping(ctx):
+  await ctx.send(f'Pong! `{round(self.bc.latency * 1000)}ms`')
+  
 bc.run('YOUR_TOKEN_HERE')
