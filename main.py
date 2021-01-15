ae884122cd54d02808b9a3a60924fd3ecc79aa20
Bongo <70061478+RealBongoChongo@users.noreply.github.com>
2021-01-15 11:05:28 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ from discord.ext import commands
 
 bc = commands.Bot(command_prefix='#')
 
-@bc.event()
+@bc.event
 async def on_ready():
   print('bot is ready')
 
-@bc.event()
+@bc.event
 async def on_member_join(member):
   print(f'{member} has joined a server with your bot')
 
-@bc.event()
+@bc.event
 async def on_member_remove(member):
   print(f'{member} has left a server with your bot')
 
